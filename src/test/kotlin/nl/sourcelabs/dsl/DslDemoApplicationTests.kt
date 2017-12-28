69048e593d81ff37cde27b9834d01c94e42d8171
Jarno Walgemoed <jwalgemoed@bol.com>
2017-12-28 10:46:36 +0100

    Second iteration

--- src/test/kotlin/nl/sourcelabs/dsl/DslDemoApplicationTests.kt
+++ src/test/kotlin/nl/sourcelabs/dsl/DslDemoApplicationTests.kt
@@ -1,5 +1,6 @@
 package nl.sourcelabs.dsl
 
+import net.javacrumbs.jsonunit.*
 import org.junit.*
 import org.junit.Assert.*
 import org.junit.runner.*
@@ -16,64 +17,77 @@ class DslDemoApplicationTests {
     @Autowired
     private lateinit var testRestTemplate: TestRestTemplate
 
+    /**
+     * 'Classic test' -> We are replacing this with our DSL
+     */
     @Test
     fun `Test 'Hello' endpoint using a valid name`() {
         val headers = HttpHeaders()
+        headers.put("Accept", listOf("application/json"))
+
         val entity = HttpEntity<Any>(headers)
 
-        val response = testRestTemplate.exchange("/api/hello/Jim", HttpMethod.GET, entity, Greeting::class.java)
+        val response = testRestTemplate.exchange("/api/hello/Jarno", HttpMethod.GET, entity, String::class.java)
 
-        assertEquals("Hi, Jim!", response.body.message)
         assertEquals(HttpStatus.OK, response.statusCode)
+        JsonAssert.assertJsonEquals("{ \"message\": \"Hi, Jarno!\" }", response.body)
     }
 
+    /**
+     * Tests using the DSL for verifications
+     */
     @Test
     fun `Test 'Hello' with a simple DSL structure`() {
-        request {
-            path = "/api/hello/Jarno"
-            method = HttpMethod.GET
-            headers {
-                accept("application/json")
+        contract {
+            request {
+                path = "/api/hello/Jarno"
+                method = HttpMethod.GET
+                headers {
+                    accept("application/json")
+                }
             }
-            expect {
-                status = HttpStatus.OK
-                body = """{ "message": "Hi, Jarno!" }"""
-            }
-        }.execute(testRestTemplate)
-    }
-
-    @Test
-    fun `Test 'Hello' with a simple DSL structure - this test intentionally fails`() {
-        request {
-            path = "/api/hello/Jarno"
-            method = HttpMethod.GET
-            headers {
-                accept("application/xml") // Endpoint does not support XML
-            }
-            expect {
+            response {
                 status = HttpStatus.OK // We will get a 406 here because the server does not support xml
                 body = """{ "message": "Hi, Jarno!" }"""
             }
-        }.execute(testRestTemplate)
+        }.verify(testRestTemplate)
+    }
+
+    @Test
+    fun `Test 'Hello' with unsupported accept header`() {
+        contract {
+            request {
+                path = "/api/hello/Jarno"
+                method = HttpMethod.GET
+                headers {
+                    accept("application/xml")
+                }
+            }
+            response {
+                status = HttpStatus.NOT_ACCEPTABLE // not expecting a body here
+            }
+        }.verify(testRestTemplate)
     }
 
     @Test
     fun `Test 'Hello' with a simple DSL structure - String interpolation & multiline`() {
         val name = "Jarno" // Re-use the variable to make sure we compare the right values.
-        request {
-            path = "/api/hello/$name" // Interpolation here too
-            method = HttpMethod.GET
-            headers {
-                accept("application/json")
+        contract {
+            request {
+                path = "/api/hello/$name" // Interpolation here too
+                method = HttpMethod.GET
+                headers {
+                    accept("application/json")
+                }
             }
-            expect {
+            response {
                 status = HttpStatus.OK
                 body = """
                     {
                         "message": "Hi, $name!"
                     }
-                """ // Multiline string, easy to read. Interpolation can help here!
+                """ // Multiline string, easy to read + interpolation
             }
-        }.execute(testRestTemplate)
+        }.verify(testRestTemplate)
     }
 }
--- src/test/kotlin/nl/sourcelabs/dsl/TestDSL.kt
+++ src/test/kotlin/nl/sourcelabs/dsl/TestDSL.kt
@@ -5,40 +5,51 @@ import org.junit.*
 import org.springframework.boot.test.web.client.*
 import org.springframework.http.*
 
+data class Contract private constructor(val request: Request, val response: Response) {
+
+    class Builder {
+        var request = Request.Builder()
+        var response = Response.Builder()
+
+        fun build(): Contract {
+            return Contract(request.build(), response.build())
+        }
+    }
+
+    fun verify(testRestTemplate: TestRestTemplate) {
+        val entity = if (request.body != null) {
+            HttpEntity(request.body, request.headers)
+        } else {
+            HttpEntity(request.headers)
+        }
+
+        val actualResponse = testRestTemplate.exchange(request.path, request.method, entity, String::class.java)
+
+        Assert.assertEquals("Actual HTTP response code doesn't match expected value.", response.status, actualResponse.statusCode)
+        JsonAssert.assertJsonEquals(response.body, actualResponse.body)
+    }
+}
+
 class Request private constructor(
         val path: String,
         val method: HttpMethod,
         val body: String?,
-        val headers: HttpHeaders,
-        val expect: Expectations
+        val headers: HttpHeaders
 ) {
     class Builder {
         var path = ""
         var method = HttpMethod.GET
         var body: String? = null
-        var expect = Expectations.Builder()
+        var expect = Response.Builder()
         var headers = HttpHeaders()
 
         fun build(): Request {
-            return Request(path, method, body?.trimIndent(), headers, expect.build())
+            return Request(path, method, body?.trimIndent(), headers)
         }
-    }
-
-    fun execute(testRestTemplate: TestRestTemplate) {
-        val entity = if (body != null) {
-            HttpEntity(body, headers)
-        } else {
-            HttpEntity(headers)
-        }
-
-        val response = testRestTemplate.exchange(path, method, entity, String::class.java)
-
-        Assert.assertEquals("Actual HTTP response code doesn't match expected value.", expect.status, response.statusCode)
-        JsonAssert.assertJsonEquals(expect.body, response.body)
     }
 }
 
-class Expectations private constructor(
+class Response private constructor(
         val status: HttpStatus,
         val body: String?
 ) {
@@ -46,13 +57,21 @@ class Expectations private constructor(
         var status = HttpStatus.OK
         var body: String? = null
 
-        fun build(): Expectations {
-            return Expectations(status, body?.trimIndent())
+        fun build(): Response {
+            return Response(status, body?.trimIndent())
         }
     }
 }
 
-fun request(builder: Request.Builder.() -> Unit) = Request.Builder().apply(builder).build()
+fun contract(builder: Contract.Builder.() -> Unit): Contract = Contract.Builder().apply(builder).build()
+
+fun Contract.Builder.request(builder: Request.Builder.() -> Unit) {
+    this.request = Request.Builder().apply(builder)
+}
+
+fun Contract.Builder.response(builder: Response.Builder.() -> Unit) {
+    this.response = Response.Builder().apply(builder)
+}
 
 fun Request.Builder.headers(headers: HttpHeaders.() -> Unit) {
     this.headers = HttpHeaders().apply(headers)
@@ -64,8 +83,4 @@ fun HttpHeaders.accept(value: String) {
 
 fun HttpHeaders.contentType(value: String) {
     this.put(HttpHeaders.CONTENT_TYPE, listOf(value))
-}
-
-fun Request.Builder.expect(builder: Expectations.Builder.() -> Unit) {
-    this.expect = Expectations.Builder().apply(builder)
 }
